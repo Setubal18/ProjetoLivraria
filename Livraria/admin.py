8ed6ee71a6e40b02a14bc129968e3c8841d0388f
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-04-11 17:18:41 -0300

    Admin pronto

--- Livraria/admin.py
+++ Livraria/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Autor,Editora,Livro
 # Register your models here.
+admin.site.register(Autor)
+admin.site.register(Editora)
+admin.site.register(Livro)
--- Livraria/models.py
+++ Livraria/models.py
@@ -15,9 +15,10 @@ class Editora(models.Model):
 class Autor(models.Model):
     nome = models.CharField(max_length=45)
     dataNascimento = models.DateField()
-    dataMorte = models.DateField(null=True)
+    dataMorte = models.DateField(null=True, blank=True)
 
-    verbose_name_plural = 'Autores'
+    class Meta:
+        verbose_name_plural = 'Autores'
 
     def __str__(self):
         return self.nome
@@ -27,9 +28,9 @@ class Livro(models.Model):
     titulo = models.CharField(max_length=45)
     isbn = models.IntegerField()
     numero_paginas = models.IntegerField()
-    anoPublicacao = models.DateField(unique_for_year='year')
+    anoPublicacao = models.DateField()
     foto_capa = models.ImageField(upload_to='imagens')
-    valor = models.FloatField('Preço')
+    valor = models.FloatField('Preço', blank=False)
     editora = models.ForeignKey(Editora, on_delete=models.CASCADE, blank=False)
     autores = models.ManyToManyField(Autor)
 
